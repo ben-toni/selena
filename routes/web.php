1ccf1ae844c06e741e1c648788c0645a1bb86022
Entoni Bencun <entoni.bencun@gmail.com>
2021-02-17 18:39:31 +0100

    Added missing routes

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,10 @@ Route::get('/', [HomeController::class, 'index']);
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('categories', [CategoryController::class, 'index'])->name('category.index');
+    Route::post('categories', [CategoryController::class, 'store'])->name('category.store');
+    Route::put('categories/{category}', [CategoryController::class, 'update'])->name('category.update');
+    Route::delete('categories/{category}', [CategoryController::class, 'destroy'])->name('category.destroy');
+    Route::get('categories/{category}/edit', [CategoryController::class, 'edit'])->name('category.edit');
+    Route::get('categories/create', [CategoryController::class, 'create'])->name('category.create');
     Route::get('products', [ProductController::class, 'index'])->name('product.index');
 });
